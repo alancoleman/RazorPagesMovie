529435408940cbcca633422ab9dfca756dee9c4d
Alan Coleman <boomfelled@gmail.com>
2020-11-17 19:20:56 +0000

    Add new field

--- Migrations/RazorPagesMovieContextModelSnapshot.cs
+++ Migrations/RazorPagesMovieContextModelSnapshot.cs
@@ -30,7 +30,10 @@ namespace RazorPagesMovie.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<decimal>("Price")
-                        .HasColumnType("decimal(18,2)");
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("ReleaseDate")
                         .HasColumnType("datetime2");
